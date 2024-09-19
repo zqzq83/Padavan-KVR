2e1dda7c063059746604c7fef736b364e9640ddd
ztx <30760655@qq.com>
2024-09-19 20:57:32 +0800

    mod config

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -530,13 +530,13 @@ struct nvram_pair router_defaults[] = {
 	{ "ip6_wan_size", "64" },
 	{ "ip6_wan_gate", "" },
 
-	{ "ip6_dns_auto", "0" },
+	{ "ip6_dns_auto", "1" },
 	{ "ip6_dns1", "" },
 	{ "ip6_dns2", "" },
 	{ "ip6_dns3", "" },
 
-	{ "ip6_lan_auto", "0" },
-	{ "ip6_lan_addr", "" },
+	{ "ip6_lan_auto", "1" },
+	{ "ip6_lan_addr", "fc00:101:101::1" },
 	{ "ip6_lan_size", "64" },
 	{ "ip6_lan_radv", "1" },
 	{ "ip6_lan_dhcp", "1" },
